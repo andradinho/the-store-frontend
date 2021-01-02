3f8c520d1863dbf992012fab0b283bf4b8cd2147
Raphael Andrade <raphaelandrade2013@gmail.com>
2021-01-02 12:06:19 -0300

    adding header & styles

--- src/components/partials/Header/index.js
+++ src/components/partials/Header/index.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import { HeaderArea } from './styled'
 
 const Header = () => {
     return (
         <HeaderArea>
-            ...
+            <div className="container">
+                <div className="logo">
+                    <Link to="/">
+                        <span className="logo-1">A</span>
+                        <span className="logo-2">K</span>
+                        <span className="logo-3">Tech</span>
+                    </Link>
+                </div>                
+            </div>            
         </HeaderArea>
     )
 }
