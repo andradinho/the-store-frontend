d2f9beda84dfa8a6c1171fb8d45bc26fc46a8157
Raphael Andrade <raphaelandrade2013@gmail.com>
2021-01-02 13:54:24 -0300

    adding pageNotFound

---
+++ src/pages/NotFound/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Page = () => {
+    return (
+        <div>
+            <h1>Page not found.</h1>
+
+            <Link to="/">Back to HOME.</Link>
+        </div>
+    )
+}
+
+export default Page
